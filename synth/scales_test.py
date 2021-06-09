e5f234c82dc429b1bf0f1f9cf4d15aff20172bcb
Benyamin Meschede-Krasa <bmeschkra@gmail.com>
2021-06-08 22:36:50 -0600

    last commit was dud

--- synth/scales_test.py
+++ synth/scales_test.py
@@ -19,17 +19,22 @@ s.start()
 #####################
 #### SYNTH SETUP ####
 #####################
-env = pyo.Adsr(attack=0.1, decay=0.5, sustain=0, release=0.1,
-           dur=0.5, mul=0.5
+env1 = pyo.Adsr(attack=0.1, decay=0.5, sustain=1, release=0.1,
+           dur=2, mul=0.5
+)
+
+env2 = pyo.Adsr(attack=0.1, decay=0.5, sustain=1, release=0.1,
+           dur=2, mul=0.5
 )
 
 mm = pyo.Mixer(outs=1, chnls=2, time=0.025)
-osc_gamma = pyo.Osc(table=pyo.TriangleTable(),mul=env)
-osc_alpha = pyo.Sine(mul=env)
+osc_alpha = pyo.Sine(mul=env1)
+osc_beta = pyo.Osc(table=pyo.TriangleTable(),mul=env2)
 reverb = pyo.Freeverb(mm[0],size=[.79,.8],damp=.9,bal=.3).out()
 
-mm.addInput(0, osc_gamma)
-mm.addInput(1, osc_alpha)
+
+mm.addInput(0, osc_alpha)
+mm.addInput(1, osc_beta)
 mm.setAmp(0,0,0.5)
 mm.setAmp(1,0,0.5)
 
@@ -38,24 +43,39 @@ mm.setAmp(1,0,0.5)
 #### PARAMS ####
 ################
 base_alpha = 55           # reference base note in Hz (A1 = 55Hz)
-base_gamma = 55
+base_beta = 110
 oct_offset = 4      # octave offset
 chr_offset = 0      # chromatic offset
 scale = constants.scales["pentatonic2"]
-scale_step_alpha = scale_step_gamma = 0
+scale_step_alpha = scale_step_beta = 0
 
+note_types = [8,4,2,1]
 
 #########################
 #### NEURO --> SOUND ####
 #########################
 # find quantiles for each spectral band
+
+# alpha and beta are used for note selection from the above scale param
 # during playback, see which value it is nearest to an play the scale note associated with that quantile index
 alpha_qt = []
-gamma_qt=[]
+beta_qt = []
 for qt in np.linspace(0,1,len(scale)):
     quantile_all = data_generator.FEATS.quantile(qt)
     alpha_qt.append(quantile_all.alpha)
+    beta_qt.append(quantile_all.beta)
+
+#  delta and beta are used for ntoe type (i.e. duration) 
+# during playback, see which value it's nearest to and plat the note for the duration associated with the quantile index
+    
+gamma_qt=[]
+delta_qt=[]
+for qt in np.linspace(0,1,len(note_types)):
+    quantile_all = data_generator.FEATS.quantile(qt)
     gamma_qt.append(quantile_all.gamma)
+    delta_qt.append(quantile_all.delta)
+
+
 
 def get_closest_quantile(value, quantiles):
     """given a value and a list of quantiles for the distribution from which that value was drawn, 
@@ -77,26 +97,43 @@ def get_closest_quantile(value, quantiles):
     return quantile_index
 
 
-for data_point in data_generator.playback_episode():
-    # output_note (in Hz) = base * 2 ** (i / 12)
-    # where i is the number of half steps above the base
-    osc_alpha.freq = float((base_alpha + base_alpha * oct_offset) * 2 ** ((scale_step_alpha + chr_offset) / 12))
-    osc_gamma.freq = float((base_gamma + base_gamma * oct_offset) * 2 ** ((scale_step_gamma + chr_offset) / 12))
+n_sixteenth_alpha = max(note_types)
+n_sixteenth_beta = max(note_types)
 
-    print("ALPHA SYNTH")
-    print(f"Scale step:\t{scale_step_alpha}")
-    print(f"Frequency:\t{osc_alpha.freq}\n")
-    print("GAMMA SYNTH")
-    print(f"Scale step:\t{scale_step_gamma}")
-    print(f"Frequency:\t{osc_gamma.freq}\n")
-    decay = 0.5+np.random.randn()*0.5
+for data_point in data_generator.playback_features():
+    # select duration based on gamma and delta
 
-    print("DECAY")
-    print(f"Decay:\t{decay}")
+    # select duration based previous value of delta and gamma
+    if n_sixteenth_alpha ==max(note_types):
+        scale_step_alpha = scale[get_closest_quantile(data_point.alpha, alpha_qt)]
+        # output_note (in Hz) = base * 2 ** (i / 12)
+        # where i is the number of half steps above the base
+        osc_alpha.freq = float((base_alpha + base_alpha * oct_offset) * 2 ** ((scale_step_alpha + chr_offset) / 12))
+        n_sixteenth_alpha = note_types[get_closest_quantile(data_point.gamma, gamma_qt)]
+        
+        env1.play()
+        print("ALPHA SYNTH")
+        print(f"Scale step:\t{scale_step_alpha}")
+        print(f"Frequency:\t{osc_alpha.freq}")
+        print(f"note type: {n_sixteenth_alpha}\n")
+        
+    else:
+        n_sixteenth_alpha +=1
+    
+    if n_sixteenth_beta ==max(note_types):
+        scale_step_beta = scale[get_closest_quantile(data_point.beta, beta_qt)]
+        osc_beta.freq = float((base_beta + base_beta * oct_offset) * 2 ** ((scale_step_beta + chr_offset) / 12))
+        n_sixteenth_beta = note_types[get_closest_quantile(data_point.delta, delta_qt)]
 
-    env.play()
-    env.setDecay(decay)
+        env2.play()
+        print("beta SYNTH")
+        print(f"Scale step:\t{scale_step_beta}")
+        print(f"Frequency:\t{osc_beta.freq}")
+        print(f"note type: {n_sixteenth_beta}\n")
 
-    scale_step_alpha = scale[get_closest_quantile(data_point.alpha, alpha_qt)]
-    scale_step_gamma = scale[get_closest_quantile(data_point.gamma, gamma_qt)]
+        
+    else:
+        n_sixteenth_beta +=1
+
+
 s.stop()
